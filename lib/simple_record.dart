c79e36be3a6fc902704bc6817db2d1374b31723a
Himchan Park <chan150@kaist.ac.kr>
2025-01-09 14:18:56 +0900

    🔨 test on small-sized record

--- lib/simple_record.dart
+++ lib/simple_record.dart
@@ -8,5 +8,5 @@ part 'simple_record.g.dart';
 class SimpleRecord {
   late int id = isar.simpleRecords.autoIncrement();
 
-  List<int> data = List.generate(30000, (i) => i);
+  List<int> data = List.generate(1, (i) => i);
 }
